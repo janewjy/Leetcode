a207c8f6603caa4c2228e5813234eda7d8d829dd
jingya <janewjy87@gmail.com>
2015-08-27 13:06:57 -0400

    happy number

--- happyNumber.py
+++ happyNumber.py
@@ -1,3 +1,25 @@
-num = 19
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        D = {}
 
-while 
+        while n >= 1:
+        	d = 0
+        	while n > 0:
+        		d += (n % 10)**2
+        		
+        		n //= 10
+        	n = d
+        	print d
+        
+        	if n == 1:
+        		return True
+        
+        	elif n in D.keys():
+        		return False
+        		
+        	else: 
+        		D[n] = 1
