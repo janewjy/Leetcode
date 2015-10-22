5f6e83fd6ce43fefc488e19872ed6fb2e9a7b8af
jingya <janewjy87@gmail.com>
2015-10-22 11:49:26 -0400

    two more faster slolutions

--- 3Sum.py
+++ 3Sum.py
@@ -18,6 +18,58 @@ class Solution(object):
                 
         return triple
             
+    #faster solutions online
+    
+    def threeSum(self, nums):
+    count={} # number: count
+    for i in nums:
+        count[i] = count.get(i,0) + 1
+
+    ans = []
+    nsort = sorted(count.keys())
+
+    for i in xrange(len(nsort)):
+        n1 = nsort[i]
+        count[n1] -= 1
+        for j in xrange(i,len(nsort)):
+            n2 = nsort[j]
+            if count[n2] >= 1:
+                count[n2] -= 1
+                n3 = - (n1 + n2)
+                if n3>=n2 and n3 in count and count[n3]>=1:
+                    ans.append([n1, n2, n3])
+                count[n2]+=1
+        count[n1]+=1
+    return ans
+
+    def threeSum(self, nums):
+        if len(nums) <3: # deal with special input
+            return []
+        elif len(nums) == 3:
+            if sum(nums) == 0:
+                return [sorted(nums)]
+
+
+        nums = sorted(nums) # sorted, O(nlgn)
+        ans = []
+
+        for i in range(len(nums) -2):
+            j = i+1
+            k = len(nums) -1 # hence i < j < k
+
+            while j<k: # if not cross line
+                temp_sum = nums[i] + nums[j] + nums[k]
+                if temp_sum == 0:
+                    ans.append((nums[i], nums[j], nums[k]))
+
+                if temp_sum > 0: # which means we need smaller sum, move k backward, remember we sort the array
+                    k -= 1
+                else:
+                    j += 1
+
+        return list(set(tuple(ans))) # I bet this is not the best way to eliminate duplicate solutions
+        
+        
         
         
         
